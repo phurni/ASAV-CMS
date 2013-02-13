70dd0684ff782c75ff2a3978a0f60152f86b7e63
Michael Barbey <demonofshadow@gmail.com>
2013-02-13 10:49:18 +0100

    Using default design.

--- Asav/protected/config/main.php
+++ Asav/protected/config/main.php
@@ -1,14 +1,13 @@
 <?php
 
 // uncomment the following to define a path alias
-Yii::setPathOfAlias('bootstrap',realpath(dirname(__FILE__) . "../../extensions/bootstrap"));
+//Yii::setPathOfAlias('bootstrap',realpath(dirname(__FILE__) . "../../extensions/bootstrap"));
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=> realpath(dirname(__FILE__). "../../"),
 	'name'=>'ASAV Int',
-	'theme'=>'bootstrap',
 		
 	// Website configuration
 	'charset'=>'utf-8',
@@ -16,7 +15,7 @@ return array(
 	'language'=>'fr',
 
 	// preloading 'log' component
-	'preload'=>array('bootstrap', 'log'),
+	'preload'=>array('log'),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -31,16 +30,12 @@ return array(
 			'password'=>'asav',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
-			'generatorPaths'=>array('ext.bootstrap.gii'),
 		),
 		
 	),
 
 	// application components
 	'components'=>array(
-		'bootstrap'=>array(
-				'class'=>'bootstrap.components.Bootstrap',
-		),
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
@@ -50,6 +45,10 @@ return array(
 			'urlFormat'=>'path',
 			'showScriptName'=>false,
 			'rules'=>array(
+				'<action:(login|logout|contact|home)>'=>array(
+					'/<action>',
+					'caseSensitive'=>false
+				),
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
