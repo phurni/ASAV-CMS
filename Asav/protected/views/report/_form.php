56c0cf0a96e3b09034f5b5661cfb636a045f809e
barzull <jeffrey.brignoli@gmail.com>
2013-03-20 15:46:32 +0100

    random

--- Asav/protected/views/report/_form.php
+++ Asav/protected/views/report/_form.php
@@ -9,12 +9,17 @@
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'report-form',
 	'enableAjaxValidation'=>false,
-)); ?>
+	'htmlOptions'=>array('class'=>"wideFields"),
+)); 
+
+?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
-<div class="row-fluid">
+<div class="row-fluid">	
+
+
 	<div class="span4">
 		<?php echo $form->labelEx($model,'Author'); ?>
 		<?php echo $form->textField($model,'Author'); ?>
@@ -23,24 +28,25 @@
 
 	<div class="span4">
 		<?php echo $form->labelEx($model,'Child'); ?>
-		<?php echo $form->textField($model,'Child'); ?>
+		<?php echo $form->textField($model,'Child');?>
 		<?php echo $form->error($model,'Child'); ?>
 	</div>	
 
-	<div class="span4">
+	<div class="row">
 		<?php echo $form->labelEx($model,'Day'); ?>
 		<?php echo $form->textField($model,'Day'); ?>
 		<?php echo $form->error($model,'Day'); ?>
 	</div>
+</div>
+<div class="row-fluid">	
 
 
-	<div class="span1">
+	<div class="row">
 		<?php echo $form->labelEx($model,'Status'); ?>
 		<?php echo $form->textField($model,'Status'); ?>
 		<?php echo $form->error($model,'Status'); ?>
 	</div>
-	</div>
-<div class="row-fluid">	
+
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'ActionsNutricient'); ?>
@@ -85,3 +91,42 @@
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
+
+
+<?php /** @var BootActiveForm $form */
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id'=>'report-form',
+    'type'=>'inline',
+    'htmlOptions'=>array('class'=>'well'),
+
+
+)); 
+$children=CHtml::listData(Child::model()->findAll(), 'Id', 'Lastname');
+?>
+<?php echo $form->labelEx($model,'Author'); ?>
+<?php echo " ";?>
+<?php echo $form->textFieldRow($model, 'Author', array('class'=>'input-medium')); ?>
+<?php echo $form->error($model,'Author'); ?>
+<?php echo " ";?>
+
+
+<?php echo $form->labelEx($model,'Child'); ?>
+<?php echo " ";?>
+<?php echo $form->dropDownListRow($model, 'Child'); ?>
+<?php echo $form->error($model,'Child'); ?>
+<?php echo " ";?>
+
+
+<?php echo $form->labelEx($model,'Day'); ?>
+<?php echo " ";?>
+<?php echo $form->textFieldRow($model, 'Day', array('class'=>'input-small')); ?>
+<?php echo $form->error($model,'Day'); ?>
+<?php echo " ";?>
+
+<?php echo $form->labelEx($model,'Status'); ?>
+<?php echo " ";?>
+<?php echo $form->textFieldRow($model, 'Status', array('class'=>'input-small')); ?>
+<?php echo $form->error($model,'Status'); ?>
+
+ 
+<?php $this->endWidget(); ?>
