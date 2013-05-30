b692b7e941d04f73ef06fe59c22ac4ee79a2d49b
barzull <jeffrey.brignoli@gmail.com>
2013-05-30 10:31:34 +0200

    update

--- Asav/protected/views/user/index.php
+++ Asav/protected/views/user/index.php
@@ -7,8 +7,8 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Create User', 'url'=>array('create')),
-	array('label'=>'Manage User', 'url'=>array('admin')),
+	array('label'=>'Créer un Utilisateur', 'url'=>array('create')),
+
 );
 ?>
 
