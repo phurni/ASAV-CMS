3fd6647a1535cf76fe3f624d8716fd9ee8aab671
Jämes Ménétrey <james@menetrey.me>
2013-06-03 13:48:46 +0200

    Internal commit.

--- Asav/protected/views/report/index.php
+++ Asav/protected/views/report/index.php
@@ -31,11 +31,6 @@ $this->menu = array (
 <h1>Rapports</h1>
 
 <?php
-/*
- * $this->widget('zii.widgets.CListView', array( 'dataProvider'=>$dataProvider,
- * 'itemView'=>'_view', ));
- */
-
 $this->widget ( 'bootstrap.widgets.TbGridView', array (
 		'type' => 'striped bordered condensed',
 		'dataProvider' => $model->search (),
