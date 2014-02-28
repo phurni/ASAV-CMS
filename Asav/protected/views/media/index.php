7b06d1516797aa400b7522b507b7b4a5aa71be45
Pascal Hurni <pascal_hurni@fastmail.fm>
2014-02-28 18:07:56 +0100

    Show a better title for media list

--- Asav/protected/views/media/index.php
+++ Asav/protected/views/media/index.php
@@ -1,14 +1,5 @@
 <?php
-$this->breadcrumbs=array(
-	'Media',
-);
-
-
-?>
-
-<h1>Medias</h1>
-
-<?php
+$title = "Medias";
 
 $columns = array();
 $columns[] = array (
@@ -17,13 +8,18 @@ $columns[] = array (
 						'value' => '($data->author ? $data->author->Fullname : "") '
 				);
 if(isset($type) && $type == "child"){
+  $title = "Medias des enfants";
 	$columns[] = array (
 						'name' => 'Child',
 						
 						'type' => 'raw',
 						'value'=>'$data->child ? CHtml::link($data->child->Fullname, Yii::app()->createUrl("/children/view", array("id"=>$data->child->Id))) : ""'
 				);
+}else if(isset($type) && $type == "staffboard"){
+  $title = "Medias de l'espace de communication";
+
 }else if(isset($type) && $type == "childmessage"){
+  $title = "Medias des messages des enfants";
 	$columns[] = array (
 						'name' => 'Child',
 						
@@ -31,6 +27,7 @@ if(isset($type) && $type == "child"){
 						'value'=>'$data->childmessage->child ? CHtml::link($data->childmessage->child->Fullname, Yii::app()->createUrl("/children/view", array("id"=>$data->childmessage->child->Id))) : ""'
 				);
 }else if(isset($type) && $type == "report"){
+  $title = "Medias des rapports";
 	$columns[] = array (
 						'name' => 'Child',
 						
@@ -54,7 +51,15 @@ $columns[] = array (
 								'style' => 'width: 50px'
 						)
 				);
+				
+$this->breadcrumbs=array(
+	$title,
+);
+?>
 
+<h1><?php echo $title ?></h1>
+
+<?php
 $this->widget ( 'bootstrap.widgets.TbGridView', array (
 		'type' => 'striped bordered condensed',
 		'dataProvider' => $dp,
